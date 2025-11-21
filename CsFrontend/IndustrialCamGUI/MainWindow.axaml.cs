13fddb81c95112f9e259a3e2b7a1eb16bb0ab462
pabloarshid <umarsa.birvancity@gmail.com>
2025-11-21 15:02:29 -0800

    Feat(GUI): Implement XAML layout for video and controls added scaffolding functions for the Gui buttons

--- CsFrontend/IndustrialCamGUI/MainWindow.axaml.cs
+++ CsFrontend/IndustrialCamGUI/MainWindow.axaml.cs
@@ -1,11 +1,51 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives; // <--- CRITICAL for Slider events
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Threading;
+using System;
 
-namespace IndustrialCamGUI;
-
-public partial class MainWindow : Window
+namespace IndustrialCamGUI
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // --- STUB FUNCTIONS TO SATISFY THE XAML COMPILER ---
+
+        // Matches Click="OnConnect"
+        private void OnConnect(object sender, RoutedEventArgs e)
+        {
+            // Logic will be added 
+        }
+
+        // Matches ValueChanged="OnExposureChanged"
+        // Note: Requires 'using Avalonia.Controls.Primitives;'
+        private void OnExposureChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            // Logic will be added 
+        }
+
+        // Matches IsCheckedChanged="OnTriggerModeChanged"
+        private void OnTriggerModeChanged(object sender, RoutedEventArgs e)
+        {
+            // Logic will be added 
+        }
+
+        // Matches Click="OnSoftwareTrigger"
+        private void OnSoftwareTrigger(object sender, RoutedEventArgs e)
+        {
+            // Logic will be added 
+        }
+
+        // Matches Click="OnBugToggle"
+        private void OnBugToggle(object sender, RoutedEventArgs e)
+        {
+            // Logic will be added 
+        }
     }
 }
--- CsFrontend/IndustrialCamGUI/obj/Debug/net9.0/IndustrialCamGUI.AssemblyInfo.cs
+++ CsFrontend/IndustrialCamGUI/obj/Debug/net9.0/IndustrialCamGUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndustrialCamGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4941832a15f498f0c2c10f976aaea6a980021f99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2d44b2411d68493005af860e04c069f336a3af")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndustrialCamGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndustrialCamGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
